68c85c32c4cc9cd0300e75ad6b1d5890a953e328
RachelChen <iceage91@gmail.com>
2021-06-08 16:50:25 +0800

    fix: fix search function when keyword has with space at the end

--- modules/getSearchResults.js
+++ modules/getSearchResults.js
@@ -2,16 +2,15 @@ const Restaurant = require('../models/restaurant') // 載入 todo model
 require('../config/mongoose')// 這裡是連線資料庫
 
 const getSearchResults = (req, res, next) => {
-  const keyword = req.query.keyword
+  const keyword = req.query.keyword.trim().toLowerCase()
   // keyword invalid, show user error notice
-  if (!keyword.toLowerCase().trim()) {
+  if (!keyword) {
     req.results = false
     return next()
   }
   // keyword valid
   const regex = new RegExp(keyword, 'i')
   req.results = Restaurant.find({ $or: [{ name: { $regex: regex } }, { category: { $regex: regex } }] })
-  const results = req.results.lean()
   next()
 }
 module.exports = getSearchResults
