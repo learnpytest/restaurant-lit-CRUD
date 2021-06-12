ec926dad25069632fada6db09bccf18429360b30
RachelChen <iceage91@gmail.com>
2021-06-12 21:01:20 +0800

    refactor: verifySearchInputOutput.js modify code style

--- modules/verifySearchInputOutput.js
+++ modules/verifySearchInputOutput.js
@@ -1,12 +1,8 @@
 const verifySearchInputOutput = (req, res, next) => {
-  const keyword = req.query.keyword
   const results = req.results
   results.lean()
     .then(results => {
-      const length = results.length
-      if (length) {
-        req.hasResults = true
-      }
+      req.hasResults = results.length > 0 ? true : false
       next()
     }).catch(error => console.log(error))
 }
