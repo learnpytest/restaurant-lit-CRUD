c0786e60fbf07e3c05583653b7a2997676f7e1d1
RachelChen <iceage91@gmail.com>
2021-06-12 21:09:36 +0800

    feat: modify index page ui

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const config = require('./config/config')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const helpers = require('handlebars-helpers')()
 const methodOverride = require('method-override') //method-override
 
 const routes = require('./routes') //總路由器
@@ -10,7 +11,7 @@ const app = express()
 const PORT = process.env.PORT || config.port
 
 // 以下是一些設定, handlebars
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: { eq: helpers.eq } }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
