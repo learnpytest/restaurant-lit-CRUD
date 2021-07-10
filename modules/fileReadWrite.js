1913b89dc03bbd1b2627dee11593ceed2124801b
RachelChen <iceage91@gmail.com>
2021-07-10 17:23:59 +0800

    feat: modify fileReadWrite.js and importRestaurant.js

--- modules/fileReadWrite.js
+++ modules/fileReadWrite.js
@@ -2,27 +2,46 @@ const multer = require('multer');
 const fs = require('fs');
 const UPLOAD_PATH = './uploads'
 const upload = multer({ dest: UPLOAD_PATH })
-const importRestaurant = require('./importRestaurant') //資料庫模板
+const { promisify } = require('util')
+const readFile = promisify(fs.readFile)
 
-const fileReadWrite = (req, res, next) => {
+const Restaurant = require('../models/restaurant') // 資料庫模板
+
+const fileReadWrite = async (req, res, next) => {
   const file = req.file
-  fs.readFile(file.path, (err, data) => {
-    if (err) return console.log(err)
-    fs.writeFile(`${UPLOAD_PATH}/${file.originalname}`, data, (err) => {
-      if (err) return console.log(err)
-      importRestaurant(`${file.originalname}`)
-      console.log('import done')
-      fs.readdir(`${UPLOAD_PATH}`, (err, files) => {
-        if (err) throw err;
-        for (const file of files) {
-          fs.unlink(`${UPLOAD_PATH}/${file}`, err => {
-            if (err) throw err;
-          });
-        }
-      });
-      console.log('remove uploaded files')
-    })
-  })
+  try {
+    const dataBuffer = await readFile(file.path)
+    await Restaurant.insertMany(JSON.parse(dataBuffer).results)
+  } catch (err) {
+    // @Todo error handling
+    return console.log(err)
+  }
   next()
 }
+
 module.exports = fileReadWrite
+
+
+
+
+// const fileReadWrite = (req, res, next) => {
+//   const file = req.file
+//   fs.readFile(file.path, (err, data) => {
+//     if (err) return console.log(err)
+//     fs.writeFile(`${UPLOAD_PATH}/${file.originalname}`, data, (err) => {
+//       if (err) return console.log(err)
+//       importRestaurant(`${file.originalname}`)
+//       console.log('import done')
+//       fs.readdir(`${UPLOAD_PATH}`, (err, files) => {
+//         if (err) throw err;
+//         for (const file of files) {
+//           fs.unlink(`${UPLOAD_PATH}/${file}`, err => {
+//             if (err) throw err;
+//           });
+//         }
+//       });
+//       console.log('remove uploaded files')
+//     })
+//   })
+//   next()
+// }
